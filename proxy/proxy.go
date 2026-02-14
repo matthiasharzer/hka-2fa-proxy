ddda43a87fd78fca16a3d297a5a111581e7da904
Matthias Harzer <matthias.harzer03@gmail.com>
2026-02-14 19:35:47 +0100

    feat: proxy all requests instead of 2fa auth requests only

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -1,6 +1,7 @@
 package proxy
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -19,7 +20,13 @@ const (
 )
 
 func isLoginSuccessful(response *http.Response) bool {
-	return response.StatusCode == http.StatusFound // For some reason
+	isFound := response.StatusCode == http.StatusFound
+	if !isFound {
+		return false
+	}
+
+	location := response.Header.Get("Location")
+	return location == "/"
 }
 
 func isLoginPage(body string) bool {
@@ -35,12 +42,12 @@ type server struct {
 	otpGenerator otp.Generator
 	targetHost   string
 	username     string
-	client       *http.Client
-	mutex        *sync.Mutex
+	jar          *cookiejar.Jar
+	mutex        *sync.RWMutex
 }
 
 func NewServer(targetHost, username string, otpGenerator otp.Generator) (http.Handler, error) {
-	sv := &server{targetHost: targetHost, otpGenerator: otpGenerator, username: username, mutex: &sync.Mutex{}}
+	sv := &server{targetHost: targetHost, otpGenerator: otpGenerator, username: username, mutex: &sync.RWMutex{}}
 	err := sv.authenticateClient()
 	if err != nil {
 		return nil, fmt.Errorf("could not authenticate client: %w", err)
@@ -49,7 +56,7 @@ func NewServer(targetHost, username string, otpGenerator otp.Generator) (http.Ha
 }
 
 // getLoginParameters performs the initial request to get session cookies and login form parameters.
-func (s *server) getLoginParameters(client *http.Client) (url.Values, string, error) {
+func (s *server) getLoginParameters() (url.Values, string, error) {
 	// Create a request so we can set headers
 	initialURL := s.targetHost + "/"
 	req, err := http.NewRequest("GET", initialURL, nil)
@@ -59,7 +66,7 @@ func (s *server) getLoginParameters(client *http.Client) (url.Values, string, er
 	req.Header.Set("User-Agent", userAgent)
 
 	// The client's CheckRedirect is configured to stop redirects
-	resp, err := client.Do(req)
+	resp, err := s.doRequest(req)
 	if err != nil {
 		return nil, "", fmt.Errorf("initial GET request failed: %w", err)
 	}
@@ -90,7 +97,7 @@ func (s *server) getLoginParameters(client *http.Client) (url.Values, string, er
 }
 
 // submitLogin prompts for credentials and submits the login form.
-func (s *server) submitLogin(client *http.Client, params url.Values, refererURL, username, password string) (*http.Response, error) {
+func (s *server) submitLogin(params url.Values, refererURL, username, password string) (*http.Response, error) {
 	// Prepare form data for the POST request
 	formData := url.Values{}
 	formData.Set("curl", params.Get("curl"))
@@ -112,7 +119,7 @@ func (s *server) submitLogin(client *http.Client, params url.Values, refererURL,
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 
 	// Execute the request
-	resp, err := client.Do(req)
+	resp, err := s.doRequest(req)
 	if err != nil {
 		return nil, fmt.Errorf("login POST request failed: %w", err)
 	}
@@ -127,18 +134,9 @@ func (s *server) authenticateClient() error {
 	if err != nil {
 		return fmt.Errorf("could not create cookie jar: %w", err)
 	}
+	s.jar = jar
 
-	// Create a custom HTTP client
-	client := &http.Client{
-		Jar: jar,
-	}
-
-	client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
-		// This stops the client from following any redirects
-		return http.ErrUseLastResponse
-	}
-
-	loginParams, refererURL, err := s.getLoginParameters(client)
+	loginParams, refererURL, err := s.getLoginParameters()
 	if err != nil {
 		return fmt.Errorf("could not get login parameters: %w", err)
 	}
@@ -147,7 +145,7 @@ func (s *server) authenticateClient() error {
 	s.otpGenerator.WaitForNextInterval()
 	password := s.otpGenerator.Generate(time.Now())
 
-	loginResp, err := s.submitLogin(client, loginParams, refererURL, s.username, password)
+	loginResp, err := s.submitLogin(loginParams, refererURL, s.username, password)
 	if err != nil {
 		return fmt.Errorf("could not submit login: %w", err)
 	}
@@ -157,16 +155,74 @@ func (s *server) authenticateClient() error {
 		return fmt.Errorf("login failed")
 	}
 
-	s.client = client
-
 	log.Println("client authenticated successfully")
 
 	return nil
 }
 
-func (s *server) proxyRequest(w http.ResponseWriter, r *http.Request, client *http.Client) error {
+func (s *server) getHttpClient(url *url.URL) (*http.Client, error) {
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
+
+	jar, err := cookiejar.New(nil)
+	if err != nil {
+		return nil, fmt.Errorf("could not create cookie jar: %w", err)
+	}
+
+	cookies := s.jar.Cookies(url)
+	jar.SetCookies(url, cookies)
+
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+		Jar:     jar,
+	}
+
+	client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		// This stops the client from following any redirects
+		return http.ErrUseLastResponse
+	}
+
+	return client, nil
+}
+
+func (s *server) saveCookiesFromResponse(url *url.URL, resp *http.Response) {
+	cookies := resp.Cookies()
+	if len(cookies) == 0 {
+		return
+	}
+
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	for _, cookie := range cookies {
+		s.jar.SetCookies(url, []*http.Cookie{cookie})
+	}
+}
+
+func (s *server) doRequest(req *http.Request) (*http.Response, error) {
+	client, err := s.getHttpClient(req.URL)
+	if err != nil {
+		return nil, fmt.Errorf("could not get HTTP client: %w", err)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	s.saveCookiesFromResponse(req.URL, resp)
+
+	return resp, nil
+}
+
+func (s *server) proxyRequest(w http.ResponseWriter, r *http.Request) error {
+	bodyBytes, err := io.ReadAll(r.Body)
+	if err != nil {
+		return fmt.Errorf("could not read request body: %w", err)
+	}
+	bodyString := string(bodyBytes)
+
 	// Create a new request based on the original one
-	proxyReq, err := http.NewRequest(r.Method, s.targetHost+r.RequestURI, r.Body)
+	proxyReq, err := http.NewRequest(r.Method, s.targetHost+r.RequestURI, strings.NewReader(bodyString))
 	if err != nil {
 		return err
 	}
@@ -174,16 +230,24 @@ func (s *server) proxyRequest(w http.ResponseWriter, r *http.Request, client *ht
 	// Copy headers from the original request
 	for name, values := range r.Header {
 		for _, value := range values {
+			if strings.ToLower(name) == "cookie" {
+				continue
+			}
+			if strings.ToLower(name) == "referer" {
+				continue
+			}
 			proxyReq.Header.Add(name, value)
 		}
 	}
 	proxyReq.Header.Set("User-Agent", userAgent)
+	proxyReq.Header.Set("Origin", s.targetHost)
 
 	// Perform the request
-	resp, err := client.Do(proxyReq)
+	resp, err := s.doRequest(proxyReq)
 	if err != nil {
 		return err
 	}
+
 	defer resp.Body.Close()
 
 	responseBodyBytes, err := io.ReadAll(resp.Body)
@@ -203,6 +267,14 @@ func (s *server) proxyRequest(w http.ResponseWriter, r *http.Request, client *ht
 		}
 	}
 
+	if resp.StatusCode >= 300 && resp.StatusCode < 400 {
+		location := resp.Header.Get("Location")
+		if strings.HasPrefix(location, s.targetHost) {
+			location = strings.TrimPrefix(location, s.targetHost)
+			w.Header().Set("Location", location)
+		}
+	}
+
 	// Write the status code
 	w.WriteHeader(resp.StatusCode)
 
@@ -215,21 +287,25 @@ func (s *server) proxyRequest(w http.ResponseWriter, r *http.Request, client *ht
 }
 
 func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	s.mutex.Lock()
-	defer s.mutex.Unlock()
-
 	log.Printf("proxying request: %s %s", r.Method, r.URL.String())
 
-	err := s.proxyRequest(w, r, s.client)
+	err := s.proxyRequest(w, r)
 	if err != nil {
+		if errors.As(err, &http.ErrHandlerTimeout) {
+			http.Error(w, "request timed out: "+err.Error(), http.StatusGatewayTimeout)
+			return
+		}
+
 		err = s.authenticateClient()
 		if err != nil {
 			http.Error(w, "re-authentication failed: "+err.Error(), http.StatusBadGateway)
 			return
 		}
-		err = s.proxyRequest(w, r, s.client)
+		err = s.proxyRequest(w, r)
 		if err != nil {
 			http.Error(w, "proxy error after re-authentication: "+err.Error(), http.StatusBadGateway)
 		}
 	}
+
+	log.Printf("request proxied successfully: %s %s", r.Method, r.URL.String())
 }
